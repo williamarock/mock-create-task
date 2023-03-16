9a4a8413728665470e1e6c7b4b734bbadcbf962e
williamarock <123965023+williamarock@users.noreply.github.com>
2023-03-16 10:24:09 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -10,86 +10,11 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, func
     Josephine.setPosition(30, 40)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite52, location5) {
-    list = [
-    "Helmet",
-    "Trap >:^)",
-    "Shield",
-    "Sword"
-    ]
-    game.splash("You just got a ", list._pickRandom())
-    if (list[0]) {
-        statusbar2.setBarSize(40, 4)
-        statusbar2.attachToSprite(Josephine)
-    }
-    if (list[1]) {
-        statusbar2.value += -15
-    }
-    if (list[2]) {
-        statusbar2.setBarSize(40, 4)
-        statusbar2.attachToSprite(Josephine)
-    }
+    let list: number[] = []
+    workChest(list, Josephine)
     tiles.setTileAt(location5, sprites.dungeon.chestOpen)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    doAttack(list, Josephine, Hard_Boss)
-})
-statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
-    game.gameOver(true)
-})
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite3, location3) {
-    statusbar2.value += -1
-})
-statusbars.onZero(StatusBarKind.Health, function (status) {
-    game.gameOver(false)
-})
-function do_Main (Josephine: Sprite) {
-    let list2 : string[] = []
-scene.setBackgroundColor(11)
-    Josephine = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f e e f f f . . . . 
-        . . . f f e e e e e e f f . . . 
-        . . f f e e e e e e e e f f . . 
-        . . f e e e e e e e e e e f . . 
-        . . f e e e e e e e e e e f . . 
-        . . f e e f f e e f f e e f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f 4 4 f 1 4 e e f . 
-        . . f e e 4 4 f f 4 4 e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Player)
-    controller.moveSprite(Josephine)
-    Josephine.setPosition(10, 125)
-    scene.cameraFollowSprite(Josephine)
-    tiles.setCurrentTilemap(tilemap`Start level`)
-    statusbar2 = statusbars.create(20, 4, StatusBarKind.Health)
-    statusbar2.attachToSprite(Josephine)
-}
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenEast, function (sprite4, location4) {
-    tiles.setCurrentTilemap(tilemap`Corridor`)
-    Josephine.setPosition(10, 125)
-    game.showLongText("Welcome to the Dungeons! Get through the maze and beat the final boss to get out and win the game! Good Luck!!!", DialogLayout.Bottom)
-})
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
-    statusbar2.value += -20
-})
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorLockedEast, function (sprite6, location6) {
-    tiles.setCurrentTilemap(tilemap`Boss Room`)
-    Josephine.setPosition(150, 60)
-    statusbar = statusbars.create(60, 4, StatusBarKind.EnemyHealth)
-    Hard_Boss = sprites.create(assets.image`Hard Boss`, SpriteKind.Enemy)
-    Hard_Boss.setPosition(70, 100)
-    statusbar.setColor(7, 2)
-    statusbar.attachToSprite(Hard_Boss)
-    Hard_Boss.follow(Josephine, 50)
-})
-function doAttack (list2: any[], Josephine: Sprite, Hard_Boss: Sprite) {
-    Attack = true
     x = Josephine.x
     y = Josephine.y
     sprites.destroy(Josephine)
@@ -146,14 +71,84 @@ function doAttack (list2: any[], Josephine: Sprite, Hard_Boss: Sprite) {
     statusbar2.attachToSprite(Josephine)
     Hard_Boss.follow(Josephine, 50)
     Attack = false
-    return 0
+})
+statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
+    game.gameOver(true)
+})
+function workChest (list: string[], Josephine: Sprite) {
+    list = [
+    "Helmet",
+    "Trap >:^)",
+    "Shield",
+    "Sword"
+    ]
+    game.splash("You just got a ", list._pickRandom())
+    if (list[0]) {
+        statusbar2.setBarSize(40, 4)
+        statusbar2.attachToSprite(Josephine)
+    }
+    if (list[1]) {
+        statusbar2.value += -15
+    }
+    if (list[2]) {
+        statusbar2.setBarSize(40, 4)
+        statusbar2.attachToSprite(Josephine)
+    }
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite3, location3) {
+    statusbar2.value += -1
+})
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    game.gameOver(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenEast, function (sprite4, location4) {
+    tiles.setCurrentTilemap(tilemap`Corridor`)
+    Josephine.setPosition(10, 125)
+    game.showLongText("Welcome to the Dungeons! Get through the maze and beat the final boss to get out and win the game! Good Luck!!!", DialogLayout.Bottom)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    statusbar2.value += -20
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorLockedEast, function (sprite6, location6) {
+    tiles.setCurrentTilemap(tilemap`Boss Room`)
+    Josephine.setPosition(150, 60)
+    statusbar = statusbars.create(60, 4, StatusBarKind.EnemyHealth)
+    Hard_Boss = sprites.create(assets.image`Hard Boss`, SpriteKind.Enemy)
+    Hard_Boss.setPosition(70, 100)
+    statusbar.setColor(7, 2)
+    statusbar.attachToSprite(Hard_Boss)
+    Hard_Boss.follow(Josephine, 50)
+})
+let Attack = false
+let Hard_Boss: Sprite = null
+let statusbar: StatusBarSprite = null
 let y = 0
 let x = 0
-let Attack = false
-let statusbar: StatusBarSprite = null
-let Hard_Boss: Sprite = null
 let statusbar2: StatusBarSprite = null
-let list: string[] = []
 let Josephine: Sprite = null
-do_Main(Josephine)
+let list2 : string[] = []
+scene.setBackgroundColor(11)
+Josephine = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f e e f f f . . . . 
+    . . . f f e e e e e e f f . . . 
+    . . f f e e e e e e e e f f . . 
+    . . f e e e e e e e e e e f . . 
+    . . f e e e e e e e e e e f . . 
+    . . f e e f f e e f f e e f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f 4 4 f 1 4 e e f . 
+    . . f e e 4 4 f f 4 4 e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Josephine)
+Josephine.setPosition(10, 125)
+scene.cameraFollowSprite(Josephine)
+tiles.setCurrentTilemap(tilemap`Start level`)
+statusbar2 = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar2.attachToSprite(Josephine)
